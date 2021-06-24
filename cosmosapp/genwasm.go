9d0ee670ea0294bf23af2be5ca5cf07e8ec9260d
ltacker <lucas.bertrand.22@gmail.com>
2021-06-24 15:51:54 +0200

    go mod init =

---
+++ cosmosapp/genwasm.go
@@ -0,0 +1,48 @@
+package cosmoscmd
+
+import (
+	"github.com/CosmWasm/wasmd/x/wasm"
+	wasmcli "github.com/CosmWasm/wasmd/x/wasm/client/cli"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/spf13/cobra"
+	"strings"
+)
+
+func AddGenesisWasmMsgCmd(defaultNodeHome string) *cobra.Command {
+	txCmd := &cobra.Command{
+		Use:                        "add-wasm-genesis-message",
+		Short:                      "Wasm genesis subcommands",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	genesisIO := wasmcli.NewDefaultGenesisIO()
+
+	txCmd.AddCommand(
+		wasmcli.GenesisStoreCodeCmd(defaultNodeHome, genesisIO),
+		wasmcli.GenesisInstantiateContractCmd(defaultNodeHome, genesisIO),
+		wasmcli.GenesisExecuteContractCmd(defaultNodeHome, genesisIO),
+		wasmcli.GenesisListContractsCmd(defaultNodeHome, genesisIO),
+		wasmcli.GenesisListCodesCmd(defaultNodeHome, genesisIO),
+	)
+
+	return txCmd
+}
+
+// GetEnabledProposals parses the ProposalsEnabled / EnableSpecificProposals values to
+// produce a list of enabled proposals to pass into wasmd app.
+func GetEnabledProposals(proposalsEnabled, enableSpecificProposals string) []wasm.ProposalType {
+	if enableSpecificProposals == "" {
+		if proposalsEnabled == "true" {
+			return wasm.EnableAllProposals
+		}
+		return wasm.DisableAllProposals
+	}
+	chunks := strings.Split(enableSpecificProposals, ",")
+	proposals, err := wasm.ConvertToProposals(chunks)
+	if err != nil {
+		panic(err)
+	}
+	return proposals
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/tendermint/spm
 
 go 1.16
+
+require (
+	github.com/CosmWasm/wasmd v0.16.0
+	github.com/cosmos/cosmos-sdk v0.42.6
+	github.com/spf13/cobra v1.1.3
+)
+
+replace google.golang.org/grpc => google.golang.org/grpc v1.33.2
+
+replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
